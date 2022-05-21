55bd1338853f93e80ad748807efaa55c37ca5342
christy313 <christycreativity@gmail.com>
2022-05-21 15:15:59 -0400

    deploy heroku

--- config/config.js
+++ config/config.js
@@ -1,27 +1,26 @@
 require("dotenv").config();
-
 const config = {
   development: {
-    username: "process.env.DEV_USERNAME",
-    password: "process.env.DEV_PASSWORD",
-    database: "process.env.DEV_DATABASE",
-    host: "process.env.DEV_HOST",
-    dialect: "mysql",
+    username: process.env.DEV_USERNAME,
+    password: process.env.DEV_PASSWORD,
+    database: process.env.DEV_DATABASE,
+    host: process.env.DEV_HOST,
+    dialect: process.env.DEV_DIALECT,
   },
   test: {
-    username: "process.env.DEV_USERNAME",
-    password: "process.env.DEV_PASSWORD",
-    database: "process.env.DEV_DATABASE",
-    host: "process.env.DEV_HOST",
-    dialect: "mysql",
+    username: process.env.DEV_USERNAME,
+    password: process.env.DEV_PASSWORD,
+    database: process.env.DEV_DATABASE,
+    host: process.env.DEV_HOST,
+    dialect: process.env.DEV_DIALECT,
   },
   production: {
-    username: "process.env.DB_USERNAME",
-    password: "process.env.DB_PASSWORD",
-    database: "process.env.DB_DATABASE",
-    host: "process.env.DB_HOST",
-    dialect: "mysql",
-    use_env_variable: "CLEARDB_DATABASE_URL",
+    username: process.env.PROD_USERNAME,
+    password: process.env.PROD_PASSWORD,
+    database: process.env.PROD_DATABASE,
+    host: process.env.PROD_HOST,
+    dialect: process.env.PROD_DIALECT,
+    use_env_variable: process.env.CLEARDB_DATABASE_URL,
   },
 };
 
