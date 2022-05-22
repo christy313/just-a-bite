bda258fa9a484796c5054cecdcfd6e1801c9376d
christy313 <christycreativity@gmail.com>
2022-05-21 20:22:00 -0400

    deploy heroku3

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const session = require("express-session");
 const flash = require("connect-flash");
@@ -17,7 +19,7 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use(
   session({
-    secret: "process.env.SESSION_SECRET",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
   })
