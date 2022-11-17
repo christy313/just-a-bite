52ac9304cb0c989502cd984dd9d2610f25f60a60
christy313 <christycreativity@gmail.com>
2022-11-17 16:46:29 -0500

    Change config file back to mysql

--- config/config.js
+++ config/config.js
@@ -20,8 +20,8 @@ const config = {
     password: "process.env.PROD_PASSWORD",
     database: "process.env.PROD_DATABASE",
     host: "process.env.PROD_HOST",
-    dialect: "pg",
-    use_env_variable: "process.env.DATABASE_URL",
+    dialect: "mysql",
+    use_env_variable: "process.env.CLEARDB_DATABASE_URL",
   },
 };
 module.exports = config;
