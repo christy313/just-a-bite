7e09c5b9e626b1d137f616bce552ab91b4df6717
christy313 <christycreativity@gmail.com>
2024-06-01 23:28:42 -0400

    Use env variables

--- index.js
+++ index.js
@@ -8,16 +8,12 @@ const flash = require("connect-flash");
 const app = express();
 const port = process.env.PORT || 5001;
 
-const CLEARDB_DATABASE_URL = process.env.CLEARDB_DATABASE_URL || "";
-const dbConfig = new URL(CLEARDB_DATABASE_URL);
-const [username, password] = dbConfig.auth.split(":");
-
 const sequelize = new Sequelize({
   dialect: "mysql",
-  host: dbConfig.hostname,
-  username: username,
-  password: password,
-  database: dbConfig.pathname.slice(1),
+  host: process.env.PROD_HOST,
+  username: process.env.PROD_USERNAME,
+  password: process.env.PROD_PASSWORD,
+  database: process.env.PROD_DATABASE,
   pool: {
     max: 100000,
     min: 0,
@@ -41,11 +37,11 @@ const prizeController = require("./controllers/prize");
 const userController = require("./controllers/user");
 
 const sessionStore = new MySQLStore({
-  host: dbConfig.hostname,
-  port: dbConfig.port || 3306,
-  user: username,
-  password: password,
-  database: dbConfig.pathname.slice(1),
+  host: process.env.PROD_HOST,
+  port: 3306,
+  user: process.env.PROD_USERNAME,
+  password: process.env.PROD_PASSWORD,
+  database: process.env.PROD_DATABASE,
 });
 
 app.set("view engine", "ejs");
