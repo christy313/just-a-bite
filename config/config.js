8758c64780a651a0d27ea17ff7183a16f25f0d5d
christy313 <christycreativity@gmail.com>
2024-06-02 14:06:16 -0400

    Add missing prize pic

--- config/config.js
+++ config/config.js
@@ -1,27 +1,13 @@
 require("dotenv").config();
 
 const config = {
-  development: {
-    username: process.env.DEV_USERNAME,
-    password: process.env.DEV_PASSWORD,
-    database: process.env.DEV_DATABASE,
-    host: process.env.DEV_HOST,
-    dialect: "mysql",
-  },
-  test: {
-    username: process.env.DEV_USERNAME,
-    password: process.env.DEV_PASSWORD,
-    database: process.env.DEV_DATABASE,
-    host: process.env.DEV_HOST,
-    dialect: "mysql",
-  },
   production: {
     username: process.env.PROD_USERNAME,
     password: process.env.PROD_PASSWORD,
     database: process.env.PROD_DATABASE,
     host: process.env.PROD_HOST,
     dialect: "mysql",
-    use_env_variable: "CLEARDB_DATABASE_URL",
+    use_env_variable: process.env.CLEARDB_DATABASE_URL,
   },
 };
 module.exports = config;
